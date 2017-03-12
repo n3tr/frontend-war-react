3c75fa019568a4850761c877d45b173e2255af09
Jirat K <saakyz@gmail.com>
2017-03-12 19:49:51 +0700

    Make infinite scroll

--- src/pages/StargazersPage.react.js
+++ src/pages/StargazersPage.react.js
@@ -7,15 +7,40 @@ import { fetchStargazers } from '../api'
 export default class StargazersPage extends React.Component {
 
   state = {
-    stargazers: []
+    stargazers: [],
+    fetching: true,
+    currentPage: 1,
   }
 
   componentDidMount() {
+    window.onscroll = this._onScrollTpBottom.bind(this)
+
+    this.fetchStargazersList()
+  }
+
+  _onScrollTpBottom() {
+    if (this.state.fetching) {
+      return
+    }
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100) {
+      this.fetchStargazersList()
+    }
+  }
+
+  fetchStargazersList() {
+    // Set Loading state
+    this.setState({
+      fetching: true
+    })
     const { ownerName, repoName } = this.props.match.params
     const scope = `${ownerName}/${repoName}`
-    fetchStargazers(scope)
+    fetchStargazers(scope, this.state.currentPage)
     .then((stargazers) => {
-      this.setState({ stargazers })
+      this.setState({
+        stargazers: this.state.stargazers.concat(stargazers),
+        fetching: false,
+        currentPage: this.state.currentPage + 1
+      })
     })
   }
 
@@ -27,6 +52,7 @@ export default class StargazersPage extends React.Component {
           jQuery JavaScript Library
         </p>
         <StargazerList stargazers={this.state.stargazers} />
+        { this.state.fetching ? <p>Loading...</p> : null}
       </div>
     )
   }
