41f394ba3822671dcb3219704d76c421e2dc0ccf
Jirat K <saakyz@gmail.com>
2017-03-16 17:05:37 +0700

    Fix lint

--- src/components/StargazerListItem.react.js
+++ src/components/StargazerListItem.react.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import avatarPlaceHolder from '../assets/profile.jpg'
+
 export default class StargazerListItem extends React.Component {
   render() {
     return (
       <div className="col-2 stargazer">
         <img
-          className="img-fluid stargazer-image" src={this.props.stargazer.avatar_url} />
+          className="img-fluid stargazer-image" 
+          alt=""
+          src={this.props.stargazer.avatar_url} />
       </div>
     )
   }
